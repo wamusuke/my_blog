92b3a26c9479826be11d1ed5f9a8091086b0b256
wa-ml <ywsophiasecict@gmail.com>
2023-01-24 19:00:10 +0900

    CardのホバーとLinkの追加

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -2,6 +2,7 @@ import AccessTimeOutlinedIcon from '@mui/icons-material/AccessTimeOutlined';
 import SellOutlinedIcon from '@mui/icons-material/SellOutlined';
 import { Card, CardContent, CardMedia, Typography, Grid } from '@mui/material';
 import type { NextPage } from 'next';
+import Link from 'next/link';
 import type { Blog, Tag } from 'src/types/blog';
 
 // Props（blogs）の型
@@ -15,49 +16,57 @@ function displayTime(time: string): string {
   return timeSplit[0] + '/' + timeSplit[1] + '/' + timeSplit[2].split('T')[0];
 }
 
-function restrictDisplayOverview(overview: string): string {
-  overview === 'string' ? overview : '';
-  if (overview.length >= 42) {
-    return overview.substring(0, 42) + '...';
+function restrictDisplayTitle(title: string): string {
+  if (title.length >= 35) {
+    return title.substring(0, 35) + '...';
   } else {
-    return overview;
+    return title;
   }
 }
+
+const CardStyle = {
+  maxWidth: 345,
+  transition: 'all 0.2s',
+  '&:hover': {
+    boxShadow:
+      '1px 0px 5px -1px rgba(0,0,0,0.1), 0px 0px 5px 5px rgba(0,0,0,0.1), 0px 1px 5px 0px rgba(0,0,0,0.1)',
+    transform: 'translateY(-1px)',
+  },
+};
 
 const BlogCard: NextPage<Props> = ({ blog, tags }: Props) => {
   const tagList = tags.map((tag) => tag.tag);
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        sx={{ height: 140 }}
-        image={`/images/thumbnails/${blog.thumbnail}`}
-        title='thumbnail'
-      />
-      <CardContent>
-        <Typography gutterBottom variant='h5' component='div'>
-          {blog.title}
-        </Typography>
-        <Typography gutterBottom variant='body2' component='div' color={'text.secondary'}>
-          {restrictDisplayOverview(typeof blog.overview === 'string' ? blog.overview : '')}
-        </Typography>
-      </CardContent>
-      <Grid container>
-        <Grid item xs={1.5}>
-          <SellOutlinedIcon />
+    <Link href={`/blog/${blog.id}`} passHref style={{ textDecoration: 'none' }}>
+      <Card sx={CardStyle}>
+        <CardMedia
+          sx={{ height: 140 }}
+          image={`/images/thumbnails/${blog.thumbnail}`}
+          title='thumbnail'
+        />
+        <CardContent>
+          <Typography gutterBottom variant='h5' component='div' sx={{ height: 100 }}>
+            {restrictDisplayTitle(blog.title)}
+          </Typography>
+        </CardContent>
+        <Grid container sx={{ height: 50 }}>
+          <Grid item xs={1.5}>
+            <SellOutlinedIcon />
+          </Grid>
+          <Grid item xs={9.5}>
+            {tagList.map((tag) => `#${tag} `)}
+          </Grid>
         </Grid>
-        <Grid item xs={9.5}>
-          {tagList.map((tag) => `#${tag} `)}
+        <Grid container>
+          <Grid item xs={1.5}>
+            <AccessTimeOutlinedIcon />
+          </Grid>
+          <Grid item xs={9.5}>
+            {displayTime(blog.createdAt)}
+          </Grid>
         </Grid>
-      </Grid>
-      <Grid container>
-        <Grid item xs={1.5}>
-          <AccessTimeOutlinedIcon />
-        </Grid>
-        <Grid item xs={9.5}>
-          {displayTime(blog.createdAt)}
-        </Grid>
-      </Grid>
-    </Card>
+      </Card>
+    </Link>
   );
 };
 
