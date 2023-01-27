c828a357ba312105432d2e9e0e62ff0f8a57089f
wa-ml <ywsophiasecict@gmail.com>
2023-01-28 03:14:20 +0900

    paginationの追加

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import Pagination from '@mui/material/Pagination';
+
+export type Props = {
+  page: number
+  setPage: React.Dispatch<number>
+  totalItemSize: number
+  sizePerPage: number
+}
+
+export default function PaginationControlled({ page, setPage, totalItemSize, sizePerPage }: Props) {
+  const maxPage = Math.ceil(totalItemSize / sizePerPage);
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  };
+
+  return (
+    <Pagination count={maxPage} page={page} onChange={handleChange} />
+  );
+}
